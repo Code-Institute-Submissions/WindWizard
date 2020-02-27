eea2ec8cd4705122688aa4b76e795ff64748aa65
jourm <jourm91@gmail.com>
2020-02-27 16:15:48 +0100

    create constructor function for weather data that assembles relevamt information from api to an array of arrays

--- assets/js/worldWeatherOnline.js
+++ assets/js/worldWeatherOnline.js
@@ -1,15 +1,15 @@
 var baseURL =  'https://api.worldweatheronline.com/premium/v1/';
 var key = wwokey;
-function inputBuilder(place){
-    this.query = place;
-    this.format = "json";
-    this.fx = 'yes'
-};
+
+var wWOData = new weatherDataMatrix();
+
 
 document.addEventListener("DOMContentLoaded", function() {
-    var test = new inputBuilder("48.834,2.394");
-    console.log(test.query);
+    var test = new inputBuilder("48.834,2.394"); /* Hardcode location */
+    
     JSONP_MarineWeather(test); 
+    $("#weatherDisplay").html(wWOData);
+    console.log(wWOData)
      
 });
 
@@ -19,7 +19,7 @@ function JSONP_MarineWeather(input) {
 
     jsonP(url, input.callback);
 }
-
+/* this function is based on example in WWO documentation and then modified to assemble the relevant data into a weatherDataMatrix object */
 function jsonP(url, callback) {
     $.ajax({
         type: 'GET',
@@ -29,10 +29,38 @@ function jsonP(url, callback) {
         jsonpCallback: callback,
         dataType: 'jsonp',
         success: function (json) {
-            console.dir('success');
+            console.dir('success');      
+            console.log(json.data.weather);
+            var i;
+            var j;
+            for (i = 0; i < json.data.weather.length; i++) {
+                for (j = 0; j < json.data.weather[i].hourly.length; j++){
+                    wWOData.add(json.data.weather[i].date, json.data.weather[i].hourly[j].time, json.data.weather[i].hourly[j].tempC, json.data.weather[i].hourly[j].weatherDesc[0].value,
+                        json.data.weather[i].hourly[j].precipMM, json.data.weather[i].hourly[j].windspeedKmph, json.data.weather[i].hourly[j].windDegree, json.data.weather[i].hourly[j].WindGustKpmh,
+                        json.data.weather[i].hourly[j].swellHeight_m, json.data.weather[i].hourly[j].waterTemp_C);
+                    
+
+                };
+            };
         },
         error: function (e) {
             console.log(e.message);
         }
     });
 }
+
+/*function to build request input */
+function inputBuilder(place){
+    this.query = place;
+    this.format = "json";
+    this.fx = 'yes';
+    this.callback;
+};
+/* Class to asaseble and store the relevant data that has been oicked out from api raw data */
+
+function weatherDataMatrix(){
+    this.data =[['day', 'time', 'temp', 'weather-description','precipitation', 'windspeed', 'wind-degree', 'wind-gust', 'swell-height', 'water-temp']];
+    this.add = function(day, time, temp, weatherDescription, precipitation , windSpeed, windDegree, windGust, swellHeight, waterTemp){
+        this.data.push([day, time, temp, weatherDescription, precipitation, windSpeed, windDegree, windGust, swellHeight, waterTemp]);
+    };
+};
