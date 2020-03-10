dc8cc94997dc883c5e7601ae88135378f1253259
jourm <jourm91@gmail.com>
2020-03-10 22:16:34 +0100

    Add autocomplete to google maps window

--- assets/js/map4.js
+++ assets/js/map4.js
@@ -20,15 +20,36 @@ function initMap() {
             lng: -7.650
         }
     });
-
-
     // Insert this overlay map type as the first overlay map type at
     // position 0. Note that all overlay map types appear on top of
     // their parent base map.
     map.overlayMapTypes.insertAt(
         0, new CoordMapType(new google.maps.Size(256, 256)));
 
-    map.addListener('click', function(e){
-       $('#lat-long').val(e.latLng.lat() + "," + e.latLng.lng());
+    map.addListener('click', function (e) {
+        $('#lat-long').val(e.latLng.lat() + "," + e.latLng.lng());
+    });
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+    autocomplete.addListener('place_changed', function () {
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(13); 
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
     });
 }
