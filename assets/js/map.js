298893d37d68ea6bd3dd0b80104060a2c21696cf
jourm <jourm91@gmail.com>
2020-03-17 15:37:07 +0100

    Remove map.js , map2.js, map3.js ad rename map4.js to map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,80 +1,71 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var map;
+// this document was copied from multiple sections of googlemaps documentation for javaScript and modified
+function CoordMapType(tileSize) {
+    this.tileSize = tileSize;
+}
+// function to assemble Windmap Url for the current tile, and add the corresponding image
+// as a backgroundimage to a div that can later be layered ontop of map tiles
+CoordMapType.prototype.getTile = function (coord, zoom, ownerDocument) {
+    var div = ownerDocument.createElement('div');
+    var windurl = "https://tile.openweathermap.org/map/wind_new/" + zoom + "/" + coord.x + "/" + coord.y + ".png?appid=" + owkey;
+    div.style.width = this.tileSize.width + 'px';
+    div.style.height = this.tileSize.height + 'px';
+    div.style.backgroundImage = "url('" + windurl + "')";
+    return div;
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map-div"), {
-            zoom: 3,
-            center: {
-                lat: 50,
-                lng: 0
-            },
-            streetViewControl: false,
-            mapTypeControlOptions: {
-                mapTypeIds: ['mywindlayer']
-            }
-        });
-        var imgMapLayer = new google.maps.ImageMapType({
-            getTileUrl: getTile(),
-            tileSize: new google.maps.Size(256, 256),
-            maxZoom: 9,
-            minZoom: 0,
-            radius: 1738000,
-            name: 'mywindlayer',
-        });
-        map.mapTypes.set('mywindlayer', imgMapLayer);
-        map.setMapTypeId('mywindlayer');
-    };
+};
 
-    /*     var infowindow = new google.maps.InfoWindow();
-        google.maps.event.addListener(map, 'idle', checkIfDataRequested);
-        map.data.addListener("click", function (event) {
-            infowindow.setContent("hej hopp")
-            infowindow.setOptions({
-                position: {
-                    lat: event.latLng.lat(),
-                    lng: event.latLng.lng()
-                },
-                pixelOffset: {
-                    width: 0,
-                    height: -15
-                }
-            });
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map-div'), {
+        zoom: 3,
+        center: {
+            lat: 41.850,
+            lng: -0.650
+        },
+        mapTypeControl: false
+    });
+    // Insert this overlay map type as the first overlay map type at
+    // position 0. Note that all overlay map types appear on top of
+    // their parent base map.
+    map.overlayMapTypes.insertAt(
+        0, new CoordMapType(new google.maps.Size(256, 256)));
+    // Function that responds to a click on the map by filling out the lat,long seacrh field with corresponding coordinates
+    map.addListener('click', function (e) {
+        $('#lat-long').val(e.latLng.lat() + "," + e.latLng.lng());
+    });
 
-        }); */
-    // the following functions were adapted from
-    // https://developers.google.com/maps/documentation/javascript/examples/map-coordinates
-    var TILE_SIZE = 256;
+    // Add a Autocomplete search bar to the top left of the map, when a place is selected,
+    // the forecast for that location is collected and displayed
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+    autocomplete.addListener('place_changed', function () {
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(13);
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        if ($("#source").children("option:selected").val() == 1) {
+            var worldWeatherOnlineData = new weatherDataMatrix;
+            var location = place.geometry.location.lat() + "," + place.geometry.location.lng();
+            var input = new inputBuilder(location);
+            JSONP_MarineWeather(input, worldWeatherOnlineData);
 
-    
-
-    function project() {
-        var siny = Math.sin(map.getCenter().lat() * Math.PI / 180);
-
-        // Truncating to 0.9999 effectively limits latitude to 89.189. This is
-        // about a third of a tile past the edge of the world tile.
-        siny = Math.min(Math.max(siny, -0.9999), 0.9999);
-
-        return new google.maps.Point(
-            TILE_SIZE * (0.5 + map.getCenter().lng() / 360),
-            TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)));
-    }
-
-    function getWindLayer(tilecords) {
-        var windmapurl = "https://tile.openweathermap.org/map/wind_new/" + map.getZoom() + "/" + tilecords.x + "/" + tilecords.y + ".png?appid=" + owkey;
-        console.log(windmapurl);
-        return windmapurl;
-    }
-    function getTile() {
-        var scale = 1 << map.getZoom();
-        var worldCoords = project();
-        var tileCoordinate = new google.maps.Point(
-            Math.floor(worldCoords.x * scale / TILE_SIZE),
-            Math.floor(worldCoords.y * scale / TILE_SIZE));
-        windLayerURL = getWindLayer(tileCoordinate);
-        return windLayerURL;
-    }
-
-    initMap()
-
-    /* map.overlayMapTypes.insertAt(0, imgMapLayer) */
-})
+        } else if ($("#source").children("option:selected").val() == 2) {
+            var openWeatherData = new weatherDataMatrix;
+            var location = place.geometry.location.lat() + "," + place.geometry.location.lng();
+            var input = new inputBuilder(location);
+            JSONP_openWeather(input, openWeatherData);
+        };
+    });
+}
