629688ec71e2c5383b870ab886fe6a59e846ab43
jourm <jourm91@gmail.com>
2020-03-18 14:40:30 +0100

    Add error handeling for OpenWeather, displays an alert upon error

--- assets/js/openWeather.js
+++ assets/js/openWeather.js
@@ -10,23 +10,23 @@ Inputs
 weather: weatherdata recived from worldweatheronline API
 dataObject: a empty weatherdatamatrix object.
   */
-function sortOpenWeatherData(weather ,dataObject){
+function sortOpenWeatherData(weather, dataObject) {
     var i = 0;
-    
-    for (i = 0; i < weather.length; i++) { 
+
+    for (i = 0; i < weather.length; i++) {
         if (weather[i].hasOwnProperty('rain') == true) {
             dataObject.add(weather[i].valueOf().dt_txt.split(" ")[0], weather[i].valueOf().dt_txt.split(" ")[1].split(":")[0], Math.round(weather[i].valueOf().main.temp - 273),
-            weather[i].valueOf().weather[0].description, weather[i].rain["3h"], weather[i].valueOf().wind.speed, weather[i].wind.valueOf().deg,
+                weather[i].valueOf().weather[0].description, weather[i].rain["3h"], weather[i].valueOf().wind.speed, weather[i].wind.valueOf().deg,
                 "no data", "no data", "no data", "http://openweathermap.org/img/wn/" + weather[i].weather[0].icon + "@2x.png");
-            
-        }else if(weather[i].hasOwnProperty('snow') == true){
+
+        } else if (weather[i].hasOwnProperty('snow') == true) {
             dataObject.add(weather[i].valueOf().dt_txt.split(" ")[0], weather[i].valueOf().dt_txt.split(" ")[1].split(":")[0], Math.round(weather[i].valueOf().main.temp - 273),
-            weather[i].valueOf().weather[0].description, weather[i].snow["3h"], weather[i].valueOf().wind.speed, weather[i].wind.valueOf().deg,
+                weather[i].valueOf().weather[0].description, weather[i].snow["3h"], weather[i].valueOf().wind.speed, weather[i].wind.valueOf().deg,
                 "no data", "no data", "no data", "http://openweathermap.org/img/wn/" + weather[i].weather[0].icon + "@2x.png");
-       
-        }else {
+
+        } else {
             dataObject.add(weather[i].valueOf().dt_txt.split(" ")[0], weather[i].valueOf().dt_txt.split(" ")[1].split(":")[0], Math.round(weather[i].valueOf().main.temp - 273),
-            weather[0].valueOf().weather[0].description, "0", weather[i].valueOf().wind.speed, weather[i].valueOf().wind.deg,
+                weather[0].valueOf().weather[0].description, "0", weather[i].valueOf().wind.speed, weather[i].valueOf().wind.deg,
                 "no data", "no data", "no data", "http://openweathermap.org/img/wn/" + weather[i].weather[0].icon + "@2x.png");
         };
     };
@@ -43,16 +43,15 @@ function jsonP1(url, callback, dataObject) {
         jsonpCallback: callback,
         dataType: 'jsonp',
         success: function (json) {
-            sortOpenWeatherData(json.list ,dataObject);
-            
+            sortOpenWeatherData(json.list, dataObject);
+
             dataObject.generateCards();
             dataObject.display();
         },
-        error: function (e) {
-            console.log(e.message);
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            alert('Error calling api for weatherforecast' + errorMessage);
         }
-        
+
     });
 }
-
-
